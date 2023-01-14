fbb7dc77ec0ae5b61cbba7f68b60c90a31c3b688
userzahar <110730909+userzahar@users.noreply.github.com>
2023-01-14 21:00:44 +0200

    dgsdg

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
+const iframe = document.getElementById('vimeo-player');
+// console.log("🚀 f", iframe)
+const player = new Player(iframe);
+player.on('timeupdate', function ({seconds}) {
+    
 
+    
+    // localStorage.setItem("videoplayer-current-time")
+    console.log(seconds);
+});
