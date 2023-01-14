c95faa693072b6f5db9daeb185f67f7085b05231
userzahar <110730909+userzahar@users.noreply.github.com>
2023-01-14 20:11:14 +0200

    ніби готово 1 завд

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
 
+function fillGalleryBox(itemsArr) {
+  const image = itemsArr.map(img => {
+    return `
+    <div class="gallery__item">
+    <a class="gallery__item" href="${img.original}">
+    <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+    </a>
+    </div>`
+}).join('');
+  galleryContainer.innerHTML = image;
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionSelector: '.gallery__image',
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
+
+fillGalleryBox(galleryItems);
 console.log(galleryItems);
