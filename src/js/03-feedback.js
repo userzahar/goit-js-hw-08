c814a9524fc7f9dcd931b2e82eee979e5f0b9bc9
userzahar <110730909+userzahar@users.noreply.github.com>
2023-01-16 01:09:01 +0200

    приступаю до кнопик

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,27 +1,39 @@
 
 const feedbackForm = document.querySelector('.feedback-form');
+const inputForm = feedbackForm.elements.email;
+const textareaForm = feedbackForm.elements.message;
+// const submitBtn = document.querySelector('button');
+
 const objData = {
     email: '',
     message:''
 }
 const STORAGE_KEY = "feedback-form-state";
 const jsonObjData = localStorage.getItem(STORAGE_KEY);
-const parseJsonObjData = JSON.parse(jsonObjData);
+const parseJsonObjData = JSON.parse(jsonObjData) || {};
 
-feedbackForm.addEventListener('input', storageFeedback)
+feedbackForm.addEventListener('input', storageFeedback);
+feedbackForm.addEventListener('submit', )
 
 function storageFeedback(e) {
    if(e.target.name === 'email') {
        objData.email = e.target.value;
-   } else if (e.target.name === 'message') {
+    };
+    if (e.target.name === 'message') {
        objData.message = e.target.value;
     }
     localStorage.setItem(STORAGE_KEY, JSON.stringify(objData))
 }
 
-function fillMemoryData(obj) {
-    
+function fillMemoryData({email, message}) {
+    if (!jsonObjData) {
+        return;
+    } else {
+        inputForm.value = email;
+        textareaForm.value = message;
+    }
 }
+fillMemoryData(parseJsonObjData);
 
 
 
