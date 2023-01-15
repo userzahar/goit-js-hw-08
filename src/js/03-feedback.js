7e905c29b54b4f2c445d360ea0b509afd6a08be0
userzahar <110730909+userzahar@users.noreply.github.com>
2023-01-16 00:37:40 +0200

    част

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
 
+const feedbackForm = document.querySelector('.feedback-form');
+const objData = {
+    email: '',
+    message:''
+}
+const STORAGE_KEY = "feedback-form-state";
+const jsonObjData = localStorage.getItem(STORAGE_KEY);
+const parseJsonObjData = JSON.parse(jsonObjData);
+
+feedbackForm.addEventListener('input', storageFeedback)
+
+function storageFeedback(e) {
+   if(e.target.name === 'email') {
+       objData.email = e.target.value;
+   } else if (e.target.name === 'message') {
+       objData.message = e.target.value;
+    }
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(objData))
+}
+
+function fillMemoryData(obj) {
+    
+}
+
+
+
+
+
+
