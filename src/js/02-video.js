5e7e64febcd0c32bac8020c1fc517b2cf9a16974
userzahar <110730909+userzahar@users.noreply.github.com>
2023-01-15 21:26:24 +0200

    kk

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,11 +5,9 @@ const iframe = document.getElementById('vimeo-player');
 const player = new Player(iframe);
 
 const throttled = throttle(setTime, 1000);
-
 function setTime({ seconds }) {
     localStorage.setItem("videoplayer-current-time", seconds);
 };
-
 
 player.on('timeupdate', throttled);
 const currentTime = localStorage.getItem("videoplayer-current-time") || 0;
